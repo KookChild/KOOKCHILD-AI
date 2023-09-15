2f7abe7bdc67f13fa40ace7361f30bc898a08c50
Dain Yoon <yoondain99@naver.com>
2023-09-15 14:30:01 +0900

    json 파일로 return 성공

--- statistic/views.py
+++ statistic/views.py
@@ -1,35 +1,101 @@
 from django.shortcuts import render
-import cx_Oracle as cx
+# import cx_Oracle as cx
+
+from .serializers import DataFrameSerializer
 # Create your views here.
 from .models  import *
 from django.http import JsonResponse
+from rest_framework.response import Response
 from django.db import connection # settings.py 에 있는 DB 랑 연결 됨
 from django.core import serializers
+import pandas as pd
+import json
+
+'''
+같은 api, 하면 try catch 로 받기? 
+'''
+
 
 def graph_api(request):
-    print("graph_api")
-    print(request.method)
+    # print("graph_api")
+    # print(request.method)
     # GET 요청에서 파라미터 추출
     child_id = request.GET.get('child_id')
     period = request.GET.get('period')
-    type = request.GET.get('type')
-    print(child_id)
-    print(period)
-    print(type)
+    dtype = request.GET.get('type')
+    # print(type(child_id))
+    # print(period)
+    # print(dtype)
 
     '''
     conn = cx.connect("1234","10.3.3.118:1521/XE") #DB연동
     cur = conn.cursor()
     cur.execute("select amount, created_date, category,is_deposit from account_history where user_id=22 and id >= 200")
     '''
+    query = "SELECT amount, created_date, category,is_deposit FROM account_history WHERE user_id = %s and id >= %s" 
+    params = [child_id,200]
 
-    with connection.cursor() as cursor:
-        cursor.execute("select amount, created_date, category,is_deposit from account_history where user_id=22 and id >= 300")
-        results = cursor.fetchall()
-        for r in results: print(r)
+    cursor = connection.cursor()
+    #with connection.cursor() as cursor:
+        #cursor.execute("select amount, created_date, category,is_deposit from account_history where user_id=22 and id >= 300")
+    cursor.execute(sql=query,params=params)
+    results = cursor.fetchall()
+    # for r in results: print(r)
 
-    # 데이터 처리 또는 API 응답 생성
-    # 예시로 JsonResponse를 사용하여 응답을 생성합니다.
+    # # 데이터 처리 또는 API 응답 생성
+    # # 예시로 JsonResponse를 사용하여 응답을 생성합니다.
+    print("connection well done")
+    columns = [desc[0] for desc in cursor.description]
+    # print(columns)
+
+
+    # pandas 처리
+    df = pd.DataFrame(results, columns=columns)
+    # print(df) # 처음 data
+    df.dropna(inplace = True)
+    df.reset_index(drop=True, inplace = True)
+    df['YEAR']=df['CREATED_DATE'].dt.year
+    df['MONTH']=df['CREATED_DATE'].dt.month
+    df['DAY']=df['CREATED_DATE'].dt.day
+    df['YEAR'] = df['YEAR'].astype(int)
+    df['MONTH'] = df['MONTH'].astype(int)
+    df['DAY'] = df['DAY'].astype(int)
+    df.drop('CREATED_DATE' , axis = 1, inplace = True)
+
+
+    df1 = df.groupby('CATEGORY').agg(SUM_AMOUNT=('AMOUNT', 'sum'), COUNT=('CATEGORY', 'count')).reset_index()
+    df1['SUM_AMOUNT'].sum()
+    df1['SUM_AMOUNT']  = df1['SUM_AMOUNT'] /df1['SUM_AMOUNT'].sum() * 100
+    df1['SUM_AMOUNT'] = df1['SUM_AMOUNT'].round(2)
+
+    # json_data = df1.to_json(orient='list')
+    # print(type(json_data))
+    # return JsonResponse(json_data,safe=False)
+
+
+    # return 해야 하는 pandas. json 파일로 바꿔야 함
+    print(df1)
+    print("========to_dict========")
+    print(df1.to_dict(orient='list'))
+    df1 = df1.to_dict(orient='list')
+    # json_data = open()
+    # json_data = json.dumps( df1, ensure_ascii=False)
+    print("========json_data========")
+    return JsonResponse(df1, safe=False)
+
+    print("========serilaizer========")
+    serializer = DataFrameSerializer(data=df1)#.to_dict(orient='list'))
+    # serializer.is_valid(raise_exception=True)
+    print(serializer)
+
+    return Response(serializer.data)
+
+
+
+
+
+
+
 
     '''
     여기서 그래프 pandas 지지고 볶고 하기 쿼리문도 받으면 될듯...? type에 따라서 case 쓰고..? 
@@ -37,10 +103,17 @@ def graph_api(request):
     data = {
         'child_id': child_id,
         'period': period,
-        'type': type,
+        'type': dtype,
         'message': 'API 요청이 성공적으로 처리되었습니다.',
     }
+    #data = df1.to_json(force_ascii=False, orient='split')
+    # print(data)
+    #data = df1.to_json()
+    print(df1.to_dict(orient='records'))
+
+    with open('data.json', 'w') as json_file:
+        json.dump(result_data, json_file)
 
 
-
+    cursor.close()
     return JsonResponse(data)
