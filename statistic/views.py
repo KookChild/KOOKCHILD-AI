b42785c4ef4db446e6eb7c132bca0bb048c266b8
Dain Yoon <yoondain99@naver.com>
2023-09-18 15:02:44 +0900

    여러가지 수정...

--- statistic/views.py
+++ statistic/views.py
@@ -1,41 +1,39 @@
-from unittest import case
-from django.shortcuts import render
-# import cx_Oracle as cx
-
-from .serializers import DataFrameSerializer
-# Create your views here.
-from .models  import *
 from django.http import JsonResponse
-from rest_framework.response import Response
 from django.db import connection # settings.py 에 있는 DB 랑 연결 됨
-from django.core import serializers
 import pandas as pd
-# import json
 from datetime import datetime
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.decorators import api_view, authentication_classes,permission_classes
+from rest_framework import permissions
+from rest_framework.response import Response
+from .models import Users
+
+# @authentication_classes([JWTAuthentication])
+
+    
 
 period = ''
 
 def get_query():
-    # AND id >= :account_history_id
-    # AND is_deposit != 1 
-    # ORDER BY created_date 
-    # 나중에는 지워야 함
-    query = "SELECT amount, created_date, category, is_deposit " \
-        "FROM account_history " \
-        "WHERE user_id = :childid " \
-        "AND id >= :account_history_id " \
-        "AND created_date >= :start_date " \
-        "AND created_date <= :end_date " \
-        "AND is_deposit != 2" \
-        "ORDER BY created_date "
-    
-    return query
+
+    if (True):
+        query = "SELECT amount, created_date, category, is_deposit " \
+            "FROM account_history " \
+            "WHERE user_id = :childid " \
+            "AND created_date >= :start_date " \
+            "AND created_date <= :end_date " \
+            "AND is_deposit != 2" \
+            "AND category != '리워드' " \
+            "ORDER BY created_date "
+
+            #  "ORDER BY created_date " 는 없어도 되긴 함
+        
+        return query
 
 def get_params(child_id,stdtor,enddtor):
 
     params = {
         'childid':child_id,
-        'account_history_id' : '0',
         'start_date' : stdtor,
         'end_date' : enddtor
     }
@@ -76,12 +74,10 @@ def get_datetime():
 
 
 
-def get_df(cursor) -> pd:
+def get_df(cursor:connection) -> pd:
     
     results = cursor.fetchall()    
-    '''
-    pandas 처리
-    '''
+
     columns = [desc[0] for desc in cursor.description]
     data = pd.DataFrame(results, columns=columns)
 
@@ -98,12 +94,48 @@ def get_df(cursor) -> pd:
 
     return data
 
-# 연간 pie chart
-# 부모인지 자녀인지에 따라서도 여기서 나눌 수 있을듯
+
 def get_pie_chart(df:pd, dtype:int,isParent:bool):
     
     pie_df = df[df['IS_DEPOSIT']== 0] # 예금이 아닌 값들만 취하기
-    pie_df = pie_df.drop('IS_DEPOSIT' , axis = 1) # IS_DEPOSIT column 버리기
+    pie_df = pie_df.drop('IS_DEPOSIT' , axis = 1).reset_index() # IS_DEPOSIT column 버리기
+    
+
+    '''
+    세분화 안되어있는 데이터
+    '''
+    pie_chart = pie_df.drop(["YEAR", "MONTH", "DAY"], axis=1)
+    pie_chart=pie_chart.groupby("CATEGORY").agg({"AMOUNT": "sum", "CATEGORY": "count"}).rename(columns={"CATEGORY": "COUNT"})
+    pie_chart = pie_chart.reset_index()
+    
+    if(isParent):
+        pie_chart['PERCENTAGE']  = (pie_chart['AMOUNT'] /pie_chart['AMOUNT'].sum() * 100).round(2)
+        pie_chart.drop(['AMOUNT'], axis=1, inplace=True)
+        pie_chart['PERCENTAGE'] = pie_chart['PERCENTAGE'].apply(lambda x: f'{x:.2f}')
+    
+    #year_pie_chart = year_pie_chart.to_dict(orient='list')
+    pie_chart = pie_chart.to_dict(orient='index')
+    print("===========pie chart===============")
+    for key, value in pie_chart.items():
+        print(f"{key}: {value}")
+    return pie_chart
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     if(dtype == 1):# 1 : year
         yearly_grouped = pie_df.groupby('YEAR')
@@ -111,13 +143,17 @@ def get_pie_chart(df:pd, dtype:int,isParent:bool):
             'CATEGORY': 'count',
             'AMOUNT': 'sum'
         }).rename(columns={'CATEGORY': 'COUNT'}))
-
         year_pie_chart= year_pie_chart.reset_index()
-        # print(year_pie_chart)
+
         if(isParent):
             year_pie_chart['PERCENTAGE']  = (year_pie_chart['AMOUNT'] /year_pie_chart['AMOUNT'].sum() * 100).round(2)
             year_pie_chart.drop(['AMOUNT'], axis=1, inplace=True)
-        year_pie_chart = year_pie_chart.to_dict(orient='list')
+            year_pie_chart['PERCENTAGE'] = year_pie_chart['PERCENTAGE'].apply(lambda x: f'{x:.2f}')
+        
+        #year_pie_chart = year_pie_chart.to_dict(orient='list')
+        year_pie_chart = year_pie_chart.to_dict(orient='index')
+        for key, value in year_pie_chart.items():
+            print(f"{key}: {value}")
         return year_pie_chart
     
     elif(dtype == 2): # month
@@ -126,13 +162,17 @@ def get_pie_chart(df:pd, dtype:int,isParent:bool):
             'CATEGORY': 'count',
             'AMOUNT': 'sum'
         }).rename(columns={'CATEGORY': 'COUNT'}))
-
         month_pie_chart = month_pie_chart.reset_index()
+
         if(isParent):
             month_pie_chart['PERCENTAGE']  = (month_pie_chart['AMOUNT'] /month_pie_chart['AMOUNT'].sum() * 100).round(2)
             month_pie_chart.drop(['AMOUNT'], axis=1, inplace=True)
-        #print(month_pie_chart)
-        month_pie_chart = month_pie_chart.to_dict(orient='list')
+            month_pie_chart['PERCENTAGE'] = month_pie_chart['PERCENTAGE'].apply(lambda x: f'{x:.2f}')
+        
+        # month_pie_chart = month_pie_chart.to_dict(orient='list')
+        month_pie_chart = month_pie_chart.to_dict(orient='index')
+        for key, value in month_pie_chart.items():
+             print(f"{key}: {value}")
         return month_pie_chart
     
 
@@ -140,24 +180,27 @@ def get_pie_chart(df:pd, dtype:int,isParent:bool):
 def get_stack_chart(df:pd, dtype:int,isParent:bool):
     
     if (dtype == 1): # yearly 
-        # period 기간 내에서 연도별 소비 내역
+
         year_stack_chart = df.groupby(['IS_DEPOSIT','YEAR']).sum().drop(['CATEGORY', 'DAY','MONTH'],  axis = 1).sort_values(by='YEAR').reset_index()
         year_stack_chart['IS_DEPOSIT'] = year_stack_chart['IS_DEPOSIT'].replace({0: '소비', 1: '예금'})
         
         if(isParent):   
             year_stack_chart['PERCENTAGE']  = (year_stack_chart['AMOUNT'] /year_stack_chart['AMOUNT'].sum() * 100).round(2)
-            year_stack_chart.drop(['AMOUNT'], axis=1, inplace=True)        
-        year_stack_chart = year_stack_chart.to_dict(orient='list')
-
+            year_stack_chart.drop(['AMOUNT'], axis=1, inplace=True)
+            year_stack_chart['PERCENTAGE'] = year_stack_chart['PERCENTAGE'].apply(lambda x: f'{x:.2f}')
+            
+        # year_stack_chart = year_stack_chart.to_dict(orient='list')
+        year_stack_chart = year_stack_chart.to_dict(orient='index')
+        print("===========stack chart===============")
+        for key, value in year_stack_chart.items():
+             print(f"{key}: {value}")
         return year_stack_chart
     
     elif(dtype == 2): # monthly
         year_and_month = get_year_and_month() # 시작과 끝의 이 동안의 년, 월을 받음
         deposit_list = [0,1]
-        # period 기간 내에서 연도별 소비 내역
+
         month_stack_chart = df.groupby(['IS_DEPOSIT','YEAR','MONTH']).sum().drop(['CATEGORY', 'DAY'],  axis = 1).reset_index()
-        # print("=======month_stack_df=========")
-        # print(month_stack_df)
         
         for r in year_and_month :
             for dp in deposit_list:
@@ -172,9 +215,14 @@ def get_stack_chart(df:pd, dtype:int,isParent:bool):
 
         if(isParent):
             month_stack_chart['PERCENTAGE']  = (month_stack_chart['AMOUNT'] /month_stack_chart['AMOUNT'].sum() * 100).round(2)
-            month_stack_chart.drop(['AMOUNT'], axis=1, inplace=True)        
-        month_stack_chart = month_stack_chart.to_dict(orient='list')
-        
+            month_stack_chart.drop(['AMOUNT'], axis=1, inplace=True)     
+            month_stack_chart['PERCENTAGE'] = month_stack_chart['PERCENTAGE'].apply(lambda x: f'{x:.2f}')
+               
+        #month_stack_chart = month_stack_chart.to_dict(orient='list')
+        month_stack_chart = month_stack_chart.to_dict(orient='index')
+        print("===========stack chart===============")
+        for key, value in month_stack_chart.items():
+             print(f"{key}: {value}")
         return month_stack_chart
         
 
@@ -209,21 +257,30 @@ def graph_api_parent(request):
     stack_chart = get_stack_chart(df=df, dtype=dtype, isParent = True)
     return_json['PIE'] = pie_chart
     return_json['STACK'] = stack_chart
+
+    # 소수점 둘쨰자리 보이게
     return JsonResponse(return_json, safe=False)
 
 
 
-
+@authentication_classes([JWTAuthentication])
+@permission_classes([permissions.IsAuthenticated])
 def graph_api_child(request):
     global period
+    global child_id
+
+    user = request.user # 내 id 가져와야함
+    
+
+    
 
     return_json = {}
 
     '''
     GET 요청에서 파라미터 추출
     '''
-
-    child_id = request.GET.get('child_id')
+    child_id = user.id
+    #child_id = request.GET.get('child_id')
     period = request.GET.get('period') # 2022-09-23~2023-09-11
     dtype = int(request.GET.get('type')) # 1 : yearly / 2 : monthly
 
@@ -245,3 +302,10 @@ def graph_api_child(request):
     return_json['PIE'] = pie_chart
     return_json['STACK'] = stack_chart
     return JsonResponse(return_json, safe=False)
+    
+
+# public String getEmail(Authentication authentication) {
+#         CustomUserDetails principal = (CustomUserDetails)authentication.getPrincipal();
+#         return principal.getEmail();
+#     }
+
