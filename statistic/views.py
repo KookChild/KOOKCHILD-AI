a588ce933321e1ca4d8be560906efcfbd1c08eab
Dain Yoon <yoondain99@naver.com>
2023-09-20 14:18:19 +0900

    수정
    1. 쿼리문 형식 """~"""으로 수정
    2. period에서 day 안받음. 월로만 비교하게끔
    3. 그 외 day 가 있는 코드 삭제

--- statistic/views.py
+++ statistic/views.py
@@ -18,36 +18,35 @@ period = ''
 def get_query():
 
     if (True):
-        query = "SELECT amount, created_date, category, is_deposit " \
-            "FROM account_history " \
-            "WHERE user_id = :childid " \
-            "AND created_date >= :start_date " \
-            "AND created_date <= :end_date " \
-            "AND category not in ('리워드', '적금')  " \
-            "ORDER BY created_date "
+        query = """
+            SELECT amount, created_date, category, is_deposit
+            FROM account_history 
+            WHERE user_id = :childid
+            AND TO_CHAR(created_date, 'YYYY-MM') BETWEEN :start_date AND :end_date
+            AND category not in ('리워드', '적금')
+        """
 
-            #  "ORDER BY created_date " 는 없어도 되긴 함
         
         return query
     
 def get_ratio_query():
 
 
-    query = "SELECT amount, category " \
-        "FROM account_history " \
-        "WHERE user_id = :childid " \
-        "AND category not in ('예금', '리워드', '적금')  " 
-
-        #  "ORDER BY created_date " 는 없어도 되긴 함
+    query = """SELECT amount, category 
+        FROM account_history
+        WHERE user_id = :childid AND category not in ('예금', '리워드', '적금')  
+        """
     
     return query
 
-def get_params(child_id,stdtor,enddtor):
+def get_params(child_id,period):
+    stdt, enddt = map(str, period.split('~'))
+
 
     params = {
         'childid':child_id,
-        'start_date' : stdtor,
-        'end_date' : enddtor
+        'start_date' : stdt,
+        'end_date' : enddt
     }
 
     return params
@@ -61,8 +60,8 @@ def get_year():
 
 def get_year_and_month():
     stdt, enddt = map(str, period.split('~'))
-    start_date = datetime.strptime(stdt, '%Y-%m-%d')
-    end_date = datetime.strptime(enddt, '%Y-%m-%d') 
+    start_date = datetime.strptime(stdt, '%Y-%m')
+    end_date = datetime.strptime(enddt, '%Y-%m') 
     result = []
 
     # 시작 날짜부터 끝 날짜까지 루프
@@ -81,11 +80,11 @@ def get_year_and_month():
     return result
 
 
-def get_datetime():
-    stdt, enddt = map(str, period.split('~'))
-    stdtor = datetime.strptime(stdt.strip(), '%Y-%m-%d').strftime('%Y-%m-%d %H:%M:%S')
-    enddtor = datetime.strptime(enddt.strip(), '%Y-%m-%d').strftime('%Y-%m-%d %H:%M:%S')
-    return stdtor, enddtor
+# def get_datetime():
+#     stdt, enddt = map(str, period.split('~'))
+#     stdtor = datetime.strptime(stdt.strip(), '%Y-%m-%d').strftime('%Y-%m-%d %H:%M:%S')
+#     enddtor = datetime.strptime(enddt.strip(), '%Y-%m-%d').strftime('%Y-%m-%d %H:%M:%S')
+#     return stdtor, enddtor
 
 
 def get_df(cursor:connection) -> pd:
@@ -102,12 +101,9 @@ def get_df(cursor:connection) -> pd:
     try:
         data['YEAR']=data['CREATED_DATE'].dt.year.astype(int)
         data['MONTH']=data['CREATED_DATE'].dt.month.astype(int)
-        data['DAY']=data['CREATED_DATE'].dt.day.astype(int)
     except:
         data['YEAR']=0
         data['MONTH']=0
-        data['DAY']=0
-        # print(data.info())
     
 
     data.drop('CREATED_DATE', axis=1, inplace=True)
@@ -125,7 +121,7 @@ def get_pie_chart(df:pd, dtype:int,isParent:bool):
     '''
     세분화 안되어있는 데이터
     '''
-    pie_chart = pie_df.drop(["YEAR", "MONTH", "DAY"], axis=1)
+    pie_chart = pie_df.drop(["YEAR", "MONTH"], axis=1)
     pie_chart=pie_chart.groupby("CATEGORY").agg({"AMOUNT": "sum", "CATEGORY": "count"}).rename(columns={"CATEGORY": "COUNT"})
     pie_chart = pie_chart.reset_index()
     
@@ -186,7 +182,7 @@ def get_stack_chart(df:pd, dtype:int,isParent:bool):
     
     if (dtype == 1): # yearly 
 
-        year_stack_chart = df.groupby(['IS_DEPOSIT','YEAR']).sum().drop(['CATEGORY', 'DAY','MONTH'],  axis = 1).sort_values(by='YEAR').reset_index()
+        year_stack_chart = df.groupby(['IS_DEPOSIT','YEAR']).sum().drop(['CATEGORY', 'MONTH'],  axis = 1).sort_values(by='YEAR').reset_index()
         year_stack_chart['IS_DEPOSIT'] = year_stack_chart['IS_DEPOSIT'].replace({0: '소비', 1: '예금'})
         
         if(isParent):   
@@ -205,7 +201,7 @@ def get_stack_chart(df:pd, dtype:int,isParent:bool):
         year_and_month = get_year_and_month() # 시작과 끝의 이 동안의 년, 월을 받음
         deposit_list = [0,1]
 
-        month_stack_chart = df.groupby(['IS_DEPOSIT','YEAR','MONTH']).sum().drop(['CATEGORY', 'DAY'],  axis = 1).reset_index()
+        month_stack_chart = df.groupby(['IS_DEPOSIT','YEAR','MONTH']).sum().drop(['CATEGORY'],  axis = 1).reset_index()
         
         for r in year_and_month :
             for dp in deposit_list:
@@ -246,14 +242,14 @@ def graph_api_parent(request):
     period = request.GET.get('period') # 2022-09-23~2023-09-11
     dtype = int(request.GET.get('type')) # 1 : yearly / 2 : monthly
 
-    stdtor, enddtor = get_datetime() # datetime 형태로 시작, 끝 날짜를 받음. oracle 형식으로
+   
     
     
     '''
     쿼리문과 params 설정:: account_history_id 는 임의의 데이터라 나중에 삭제해야함
     ''' 
     cursor = connection.cursor()
-    cursor.execute(sql=get_query(),params=get_params(child_id,stdtor,enddtor))
+    cursor.execute(sql=get_query(),params=get_params(child_id,period))
     
     # 데이터프레임으로 받기
     df = get_df(cursor=cursor)
@@ -272,11 +268,6 @@ def get_ratio(request):
     child_id = request.GET.get('child_id')
     return_json = {}
     cursor = connection.cursor()
-
-
-
-
-
 
 
     cursor.execute("SELECT birthdate FROM Users where id = :id", {"id" : child_id})
@@ -365,14 +356,14 @@ def graph_api_child(request):
     period = request.GET.get('period') # 2022-09-23~2023-03-11
     dtype = int(request.GET.get('type')) # 1 : yearly / 2 : monthly
 
-    stdtor, enddtor = get_datetime() # datetime 형태로 시작, 끝 날짜를 받음. oracle 형식으로
+   
     
     
     '''
     쿼리문과 params 설정:: account_history_id 는 임의의 데이터라 나중에 삭제해야함
     ''' 
     cursor = connection.cursor()
-    cursor.execute(sql=get_query(),params=get_params(child_id,stdtor,enddtor))
+    cursor.execute(sql=get_query(),params=get_params(child_id,period))
     
     # 데이터프레임으로 받기
     df = get_df(cursor=cursor)
